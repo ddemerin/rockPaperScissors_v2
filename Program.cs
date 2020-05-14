f87c227aa3fd9e34518ef12c76f21097843d7e59
Daniel D Demerin <ddemerin@gmail.com>
2020-05-14 15:09:29 -0400

    started rockPaperScissors assignment again

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace rockPaperScissors_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play a game of Rock, Paper, Scissors!");
+            Console.WriteLine("Pick one: Rock, Paper, or Scissors?");
+
+            var userPick = Console.ReadLine().ToLower();
+            if (userPick != "rock" || userPick != "scissors" || userPick != "paper")
+            {
+                Console.WriteLine("That is not a valid option! Pick again!");
+                userPick = Console.ReadLine().ToLower();
+            }
+
+
+        }
+    }
+}
