82b89037b682c01a5ffb6e24df54419237e04260
Daniel D Demerin <ddemerin@gmail.com>
2020-05-14 15:26:28 -0400

    finished explorer mode

--- Program.cs
+++ Program.cs
@@ -10,13 +10,47 @@ namespace rockPaperScissors_v2
             Console.WriteLine("Pick one: Rock, Paper, or Scissors?");
 
             var userPick = Console.ReadLine().ToLower();
-            if (userPick != "rock" || userPick != "scissors" || userPick != "paper")
+            while (userPick != "rock" && userPick != "scissors" && userPick != "paper")
             {
                 Console.WriteLine("That is not a valid option! Pick again!");
                 userPick = Console.ReadLine().ToLower();
             }
 
+            var randomize = new Random();
+            string[] compPick = { "rock", "paper", "scissors" };
+            int compIndex = randomize.Next(compPick.Length);
+            var opponent = compPick[compIndex];
 
+            Console.WriteLine($"Your opponent chose {opponent}!");
+
+            if (userPick == "rock" && opponent == "rock")
+            {
+                Console.WriteLine("It's a tie!");
+            }
+            else if (userPick == "rock" && opponent == "scissors")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (userPick == "rock" && opponent == "paper")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else if (userPick == "paper" && opponent == "rock")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (userPick == "paper" && opponent == "scissors")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else if (userPick == "scissors" && opponent == "paper")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (userPick == "scissors" && opponent == "rock")
+            {
+                Console.WriteLine("You lose!");
+            }
         }
     }
 }
